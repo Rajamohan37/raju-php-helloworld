50de8f02d233d7043cbbfa8570a2fa9cf610f5db
Rajamohan Raju <24845428+Rajamohan37@users.noreply.github.com>
2021-07-20 15:17:56 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html> 
+	<head> 
+	<title>Home</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+	</head>
+
+<body> 
+	<h1>Hello World! </h1>
+		<p><b> This page discribes the demo of PHP application </b></p>
+		<?php echo "The Current Date and yes las  Time is: <br />";
+		echo date("g:i A l, F j Y.");?> </p>
+	</body> 
+</html>
